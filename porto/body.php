6e77e15c2d26d0327f91e2b1c1503529863d2f7f
Radu421 <77068402+Radu421@users.noreply.github.com>
2021-02-01 17:59:13 +0200

    update

---
+++ porto/body.php
@@ -0,0 +1,33 @@
+<html>
+
+<body>
+    <!-- Adaugare in "Schema pentru toata pagina" -->
+    <div class="container">
+        <!-- Exemplu pentru constructie  -->
+        <!-- https://getbootstrap.com/docs/4.5/layout/grid/ -->
+        <!-- S1 Sectiunea De Navigare -->
+        <?php
+        include('s1.php');
+        include('s2.php');
+        include('s3.php');
+        include('s4.php');
+        include('s5.php');
+        include('s6.php');
+        include('s7.php');
+        include('s8.php');
+        include('s9.php');
+        include('s10.php');
+        ?>
+        <!-- Inchidere Sectiunea Experienta Numar -->
+    </div>
+
+    <!-- Inchiderea Sectiunii Experienta -->
+    <!-- Inchiderea Finala -->
+    </div>
+</body>
+
+</html>
+
+<?php 
+include('script.php');
+?>
---
+++ porto/s1.php
@@ -0,0 +1,62 @@
+<div id="1" class="row">
+    <div class="col-md-12">
+        <!-- https://getbootstrap.com/docs/4.5/components/navbar/ -->
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <!-- Definim Logoul  -->
+            <a class="navbar-brand" href="#">
+                <img src="imgs/logo.png">
+            </a>
+            <!-- Definim Zona de Navigare -->
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">About Me <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Portofolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Testimonials</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                    <!-- https://getbootstrap.com/docs/4.5/components/buttons/ -->
+                    <li class="nav-item">
+                        <a type="button" class="nav-link btn btn-danger " href="#">Hire Me</a>
+                    </li>
+                </ul>
+
+                <!-- Mobile  -->
+
+                <div class="pos-f-t">
+                    <div class="collapse" id="navbarToggleExternalContent">
+                        <div class="bg-dark p-4">
+                            <h4 class="text-white">Collapsed content</h4>
+                            <span class="text-muted">Toggleable via the navbar brand.</span>
+                        </div>
+                    </div>
+                    <nav class="navbar navbar-dark bg-dark">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </nav>
+                </div>
+                <!-- <span class="navbar-text">
+                Navbar text with an inline element
+              </span> -->
+            </div>
+
+        </nav>
+
+    </div>
+
+
+    <!-- Inchidem Sectiunea 1 -->
+</div>
